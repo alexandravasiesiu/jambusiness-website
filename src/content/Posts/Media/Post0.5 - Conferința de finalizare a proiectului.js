bb8fc5fafc5278ac8bf493b6a726c9d73050dd3c
Vasiesiu Alexandra <vasiesiu.alexandra@yahoo.com>
2024-02-09 09:54:03 +0200

    content updated

---
+++ src/content/Posts/Media/Post0.5 - Conferința de finalizare a proiectului.js
@@ -0,0 +1,31 @@
+export const options = {
+    "title": "Conferința de finalizare a proiectului",
+    "category": "MEDIA",
+    "category_link": "/projectsmedia",
+    "id": 101,
+}
+
+export default function Post() {
+    return (
+        <>
+            <div class="accordion-content mt-4">
+                <p class="mb-3 w-full md:text-lg">Proiectul s-a derulat în perioada 30 iunie 2022 – 29 decembrie 2023 și a avut o valoare totală de 4.942.170,05 lei, din care finanțarea nerambursabilă a fost de 4.793.251,54 lei. Proiectul a fost finanțat prin Fondul Social European, prin Programul Operațional Capital Uman 2014-2020.</p>
+                <p class="mb-3 w-full md:text-lg">Liderul de proiect a fost Marinex SRL împreună cu Partenerul 1 Formintens Centru SRL și Partenerul 2 Jam Business SRL.</p>
+                <div class="flex items-end justify-between flex-wrap box-border py-3">
+                <a href="https://www.marinex-constructii.ro/proiect/154155/wp-content/uploads/sites/2/2024/01/154155-Prezentare-final-proiect.pdf" class="items-center text-lg flex hover:text-red-500 underline">154155-Prezentare-final-proiect</a>
+                <a href="https://www.marinex-constructii.ro/proiect/154155/wp-content/uploads/sites/2/2024/01/154155-Prezentare-final-proiect.pdf" class="bg-red-500 text-white cursor-pointer inline-block text-lg font-semibold tracking-wide leading-tight my-0 px-4 py-3 uppercase" aria-describedby="154155-Prezentare-final-proiect.pdf" download>Descarcă</a>
+                </div>
+                <p class="mb-3 w-full md:text-lg"><b>OBIECTIVUL GENERAL AL PROIECTULUI:</b></p>
+                <p class="mb-3 w-full md:text-lg">Scopul proiectului – creșterea ocupării, precum și îmbunătățirea nivelului de competențe al tinerilor NEETs șomeri cu vârsta între 16 – 29 ani, înregistrați la Serviciul Public de Ocupare, cu rezidența în regiunea Centru, prin derularea:</p>
+                <ol class="list-decimal list-inside md:px-5 mb-4 text-base md:text-lg text-coolGray-500">
+                    <li class="mb-3 w-full md:text-lg list-disc">cursurilor de formare de nivel 2-calificare in domenii cerute pe piața muncii pentru 231 persoane (patiser-80 persoane, pavator 60 persoane, lucrător în structuri pentru construcții 40 persoane și lucrător comercial 51 persoane),</li>
+                    <li class="mb-3 w-full md:text-lg list-disc">cursului de formare Gestiunea deșeurilor pentru 120 persoane,</li>
+                    <li class="mb-3 w-full md:text-lg list-disc">cursului de Competențe Antreprenoriale pentru 29 persoane,</li>
+                    <li class="mb-3 w-full md:text-lg list-disc">programelor de mediere pe piața muncii pentru 380 persoane și</li>
+                    <li class="mb-3 w-full md:text-lg list-disc">finanțarea a 16 planuri de afaceri cu sume de până la 25.000 euro</li>
+                </ol>
+                <p class="mb-3 w-full md:text-lg">Le mulțumim tuturor celor care au participat și le dorim mult succes în valorificarea acestor competențe.</p>
+            </div>
+        </>
+    )
+}
